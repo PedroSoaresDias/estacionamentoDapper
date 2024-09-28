31cd4265e686048549abf7b246ba8a0144787209
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-09-28 11:30:01 -0300

    Adicionando mais testes referentes ao Ticket

--- testes/Models/TicketTest.cs
+++ testes/Models/TicketTest.cs
@@ -22,4 +22,38 @@ public class TicketTest
         Assert.AreEqual(1, ticket.VeiculoId);
         Assert.AreEqual(1, ticket.VagaId);
     }
+
+    [TestMethod]
+    public void TestandoMetodoValorTotal()
+    {
+        // Arrange
+        var cliente = new Cliente { Id = 1, Nome = "Pedro" };
+        var veiculo = new Veiculo { Id = 1, Marca = "Volkswagen", Modelo = "Virtus", ClienteId = cliente.Id, Placa = "MIT5895" };
+        var ticket = new Ticket { DataEntrada = DateTime.Now.AddHours(-1), Id = 1, VagaId = 1, VeiculoId = veiculo.Id };
+        var valorDoMinuto = new ValorDoMinuto { Minutos = 1, Valor = 1 };
+
+        // Act
+        var valorTotal = ticket.ValorTotal(valorDoMinuto);
+
+        // Assert
+        Assert.AreEqual(60, valorTotal);
+    }
+
+    [TestMethod]
+    public void TestandoValorPagoDoTicket()
+    {
+        // Arrange
+        var cliente = new Cliente { Id = 1, Nome = "Pedro" };
+        var veiculo = new Veiculo { Id = 1, Marca = "Volkswagen", Modelo = "Virtus", ClienteId = cliente.Id, Placa = "MIT5895" };
+        var ticket = new Ticket { DataEntrada = DateTime.Now.AddHours(-1), Id = 1, VagaId = 1, VeiculoId = veiculo.Id };
+        var valorDoMinuto = new ValorDoMinuto { Minutos = 1, Valor = 1 };
+        var valorTotalDesejado = ticket.ValorTotal(valorDoMinuto);
+
+        // Act
+        ticket.Pago(valorDoMinuto);
+
+        // Assert
+        Assert.AreEqual(valorTotalDesejado, ticket.Valor);
+        Assert.IsNotNull(ticket.DataSaida);
+    }
 }
